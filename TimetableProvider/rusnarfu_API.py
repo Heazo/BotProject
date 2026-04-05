508952a751dfc78e0404861bc18019ace8c9664f
Heazo <heazo1337@gmail.com>
2026-04-05 17:40:59 +0300

    Parser 4.1

--- TimetableProvider/rusnarfu_API.py
+++ TimetableProvider/rusnarfu_API.py
@@ -8,8 +8,6 @@ import requests
 from bs4 import BeautifulSoup
 
 from Models.session import Session
-#from Models.stady_day import Stady_day
-#from Models.week import Week
 
 def requestNarfu() -> list[Session]:
     #lite-mode; hard-mode; extra-mode; ultra-extra-mod | парсим один день, парсим неделю, парсим все доступные недели, парсим весь сайт
@@ -33,16 +31,10 @@ def requestNarfu() -> list[Session]:
     else :
         print("Err get request: ", response.status_code)
         return
-    #title = str(soup.find_all('title'))
-
-    # #Парсим class="nav nav-tabs"
-    # title = soup.title.text
-    # nav_tab_weeks = soup.find_all("span", {"class": "visible-lg"})   # nav_tab_weeks[0].text - текущая неделя
-    # print("title: ",title)
-    # print("week: ",nav_tab_weeks[0].text)
 
     # with open("rusNARFU.html", "w") as file:
     #     file.write(html_result)
+
     sessions_list = []
 
     #Парсим все пары, что есть на странице
@@ -85,58 +77,6 @@ def requestNarfu() -> list[Session]:
                 )
             )
     return sessions_list
-
-
-
-#     #Парсим class="tab-content" то есть все недели
-#     weeks = soup.find_all("div" ,{"class": "row tab-pane"})
-#     weeks.insert(0, soup.find_all("div" ,{"class": "row tab-pane active"}).pop())
-#
-#     data = []   #Список списков словарей
-#
-#
-#     stady_day_list = []
-#     week_list = []
-#
-#     for week in weeks:
-#         print("id = ",week.get('id'))
-#         days = week.select('div[class^="list"]')
-#
-#         for day in days:
-#             day_date = week.find("div", {"class": "dayofweek hidden-xs hidden-sm"}).text
-#             sessions = day.select('div[class^="timetable_sheet"]')
-#
-#             for session in sessions:
-#
-#                 num_elem = session.find('span', {"class": "num_para"})
-#                 time_elem = session.find('span', {"class": "time_para"})
-#                 kind_elem = session.find('span', {"class": "kindOfWork"})
-#                 discipline_elem = session.find('span', {"class": "discipline"})
-#                 auditorium_elem = session.find('span', {"class": "auditorium"})
-#                 group_elem = session.find('span', {"class": "group"})
-#
-#                 # Пропускаем если нет важных элементов
-#                 if not all([num_elem, time_elem, discipline_elem]):
-#                     #print(f"None session")
-#                     continue
-#
-#                 sessions_list.append(
-#                     Session(
-#                         num_elem.text,
-#                         time_elem.text,
-#                         kind_elem.text if kind_elem else "",
-#                         discipline_elem.text,
-#                         auditorium_elem.text if auditorium_elem else "",
-#                         group_elem.text if group_elem else "",
-#                         day_date
-#                     )
-#                 )
-#             stady_day_list.append(Stady_day(day_date, sessions_list))
-#         week_list.append(Week(stady_day_list))
-#     return week_list
-#
-# #def pars_for_day():
-
 
 
 #Возможно на английском для БД будет лучше
