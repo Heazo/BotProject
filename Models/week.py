e63673c1626bad2b65ae5cb8e82af3a24a0e6b8c
Heazo <heazo1337@gmail.com>
2026-04-06 19:19:27 +0300

    Parser 4.2

--- Models/week.py
+++
@@ -1,6 +0,0 @@
-from stady_day import Stady_day
-
-class Week:
-    def __init__(self, days: list[Stady_day]):
-        self.days = days
-        #self.sessions = sessions
--- Models/weeks.py
+++
@@ -1,5 +0,0 @@
-from week import Week
-
-class Weeks:
-    def __init__(self, weeks):
-        self.weeks = weeks
