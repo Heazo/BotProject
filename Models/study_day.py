a1c87e75eb4a6c22a0e1d3c33b75dbb72c23c45b
VvSilv <vladimirbogomolov053@gmail.com>
2026-04-23 19:42:43 +0300

    Исправил название класса

--- Models/study_day.py
+++ Models/study_day.py
@@ -1,6 +1,6 @@
 from session import Session
 
-class Stady_day:
+class Study_day:
     def __init__(self, day_date, session_list : list[Session]):
         self.day_date = day_date
         self.sessions = session_list
